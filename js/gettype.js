c3367d3d6da1d2afa3154c2b57d0d13ec4462b4c
ACASEZ <adelin.casez@ynov.com>
2021-12-15 15:12:28 +0100

    fetch api

---
+++ js/gettype.js
@@ -0,0 +1,12 @@
+function getType() {
+    var typeList = document.getElementsByTagName('input')
+
+    let value = [];
+
+    for (i = 0; i < typeList.length; i++) {
+        if (typeList[i].checked) {
+            value.push(typeList[i].value)
+        }
+    }
+    return value;
+}
--- js/main.js
+++ js/main.js
@@ -1,13 +1,30 @@
-const { type } = require("os")
-
-var url = `` //lien api
+var url = `http://localhost:8081/data` //lien api
 
 
-function getType() {
-    var typeList = document.getElementsByTagName('input')
 
+function showmonsters() {
+    let types = getType()
+    fetch(url).then((response) =>
+        response.json().then((data) => {
+            console.log(data)
+            document.getElementById("main").innerHTML = `<div id="img-title">
+            <img src="../images/FontP.jpg" alt="Forest" style="width: 100%";>
+            <div class="title"> Les créatures d'Aniphis</div>
+        </div>`
 
-    for (i = 0; i < typeList.length; i++) {
-        console.log(typeList[i].checked)
-    }
+            for (i = 0; i < data.length; i++) {
+                if (types.includes(data[i].type) || types.length == 0) {
+                    document.getElementById("main").innerHTML +=
+                        `<div class="monsters-block" id="monsters-block">
+                    <div class="" id="monsters-description">
+                        <p>${data[i].description}</p>
+                        </div>
+                        <div class="" id="monsters-img">
+                        <img class="" src="${data[i].image}" style="max-height: 400px;" alt="" />
+                        </div>
+                       `
+                }
+            }
+        })
+    );
 }
