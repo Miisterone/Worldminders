b548afd785e11315107fb55291474aaa6d10d0a1
ACASEZ <adelin.casez@ynov.com>
2022-01-07 14:18:33 +0100

    fix on searchbar again its working well now

--- js/monsters.js
+++ js/monsters.js
@@ -1,26 +1,20 @@
 var url = `http://localhost:8081/data` //lien api
-var searchbar_position = document.getElementById("searchbar").offsetTop - 101
+var searchbar_position = document.getElementById("searchbar").offsetTop - 100
 
 let exampleDropdown = new Axentix.Dropdown('#sort-dropdown', {
     animationType: 'fade',
     animationDuration: 200
 });
 
-window.onresize = function(event) { searchbar_position = document.getElementById("searchbar").offsetTop - 101 }
+window.onresize = function(event) {
+    let container_above = document.getElementById("top-container")
+    searchbar_position = container_above.offsetTop + container_above.offsetHeight - 40;
+    SearchbarPlacement()
+
+}
 
 window.onscroll = function(event) {
-    let searchbar = document.getElementById("searchbar")
-    console.log(searchbar.offsetTop, window.scrollY);
-    if (searchbar_position <= window.scrollY) {
-        searchbar.style.position = 'fixed'
-        searchbar.style.top = '100px'
-        document.getElementById('description').style.marginTop = '50px'
-    } else {
-        searchbar.style.position = 'relative'
-        searchbar.style.top = 'auto'
-        document.getElementById('description').style.marginTop = '0px'
-
-    }
+    SearchbarPlacement()
 }
 
 
@@ -45,4 +39,18 @@ function printcontent() {
             }
         })
     );
+}
+
+function SearchbarPlacement() {
+    let searchbar = document.getElementById("searchbar")
+    console.log(searchbar_position, window.scrollY)
+    if (searchbar_position <= window.scrollY) {
+        searchbar.style.position = 'fixed'
+        searchbar.style.top = '100px'
+        document.getElementById('description').style.marginTop = '50px'
+    } else {
+        searchbar.style.position = 'relative'
+        searchbar.style.top = 'auto'
+        document.getElementById('description').style.marginTop = '0px'
+    }
 }
