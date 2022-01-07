4b0639be52abfe05c6a6218a7bb140d5b71de600
ACASEZ <adelin.casez@ynov.com>
2022-01-07 13:35:05 +0100

    fix searchbar again still a bug on resize when the searchbar is already fixed on top

--- js/monsters.js
+++ js/monsters.js
@@ -6,15 +6,16 @@ let exampleDropdown = new Axentix.Dropdown('#sort-dropdown', {
     animationDuration: 200
 });
 
+window.onresize = function(event) { searchbar_position = document.getElementById("searchbar").offsetTop - 101 }
+
 window.onscroll = function(event) {
     let searchbar = document.getElementById("searchbar")
     console.log(searchbar.offsetTop, window.scrollY);
-    if(searchbar_position <= window.scrollY) {
+    if (searchbar_position <= window.scrollY) {
         searchbar.style.position = 'fixed'
         searchbar.style.top = '100px'
-        document.getElementById('description').style.marginTop = '55px'
-    }
-    else {
+        document.getElementById('description').style.marginTop = '50px'
+    } else {
         searchbar.style.position = 'relative'
         searchbar.style.top = 'auto'
         document.getElementById('description').style.marginTop = '0px'
@@ -36,10 +37,10 @@ function printcontent() {
     fetch(url).then((response) =>
         response.json().then((data) => {
             console.log(data)
-            if (types = "Élément"){
+            if (types = "Élément") {
                 document.getElementById("main-content").innerHTML = ``
             }
-            if (types = "Région"){
+            if (types = "Région") {
                 document.getElementById("main-content").innerHTML = ``
             }
         })
