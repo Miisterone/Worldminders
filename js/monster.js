699003895cba6e5c98b7f6f204a926022d81cdc9
ACASEZ <adelin.casez@ynov.com>
2022-01-09 15:59:05 +0100

    sort element and sort region

--- js/monster.js
+++ js/monster.js
@@ -110,20 +110,38 @@ function func(id, parentId) {
     if (id.style.display == "none") {
         id.style.display = "flex"
         document.getElementById(parentId).style.borderRadius = '10px'
-        document.querySelector(`#${parentId} #collapsible-img`).style.marginLeft = '40px'
         document.querySelector(`#${parentId} #collapsible-img`).style.marginTop = '-40px'
+        if (document.querySelector(`#${parentId} .collapsible-trigger`).style.flexDirection == 'row-reverse') {
+            document.querySelector(`#${parentId} #collapsible-img`).style.marginLeft = '40px'
+            document.querySelector(`#${parentId} #collapsible-title`).style.marginRight = '40px'
+        } else {
+            document.querySelector(`#${parentId} #collapsible-img`).style.marginRight = '40px'
+            document.querySelector(`#${parentId} #collapsible-title`).style.marginLeft = '40px'
+        }
     } else {
         id.style.display = "none"
         document.getElementById(parentId).style.borderRadius = '60px'
-        document.querySelector(`#${parentId} #collapsible-img`).style.marginLeft = '0'
         document.querySelector(`#${parentId} #collapsible-img`).style.marginTop = '0'
+        if (document.querySelector(`#${parentId} .collapsible-trigger`).style.flexDirection == 'row-reverse') {
+            document.querySelector(`#${parentId} #collapsible-img`).style.marginLeft = '0'
+            document.querySelector(`#${parentId} #collapsible-title`).style.marginRight = '0'
+        } else {
+            document.querySelector(`#${parentId} #collapsible-img`).style.marginRight = '0'
+            document.querySelector(`#${parentId} #collapsible-title`).style.marginLeft = '0'
+        }
     }
 
     if (old_id && old_id != id) {
         old_id.style.display = "none"
         document.getElementById(old_parent_id).style.borderRadius = '60px'
-        document.querySelector(`#${old_parent_id} #collapsible-img`).style.marginLeft = '0'
         document.querySelector(`#${old_parent_id} #collapsible-img`).style.marginTop = '0'
+        if (document.querySelector(`#${old_parent_id} .collapsible-trigger`).style.flexDirection == 'row-reverse') {
+            document.querySelector(`#${old_parent_id} #collapsible-img`).style.marginLeft = '0'
+            document.querySelector(`#${old_parent_id} #collapsible-title`).style.marginRight = '0'
+        } else {
+            document.querySelector(`#${old_parent_id} #collapsible-img`).style.marginRight = '0'
+            document.querySelector(`#${old_parent_id} #collapsible-title`).style.marginLeft = '0'
+        }
     }
     old_id = id
     old_parent_id = parentId
