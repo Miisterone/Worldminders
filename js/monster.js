56e1c8eeda07c1c2cfcbeb63d92c678c6ce0ff45
ACASEZ <adelin.casez@ynov.com>
2022-01-09 12:59:37 +0100

    sort element and sort region

--- js/monster.js
+++ js/monster.js
@@ -1,10 +1,15 @@
 var url = `http://localhost:8081/data` //lien api
 var searchbar_position = document.getElementById("searchbar").offsetTop - 100
+var sort_content = document.getElementById("main-content")
+const array_element = ["Neutre", "Ardent", "Électrique", "Aquatique", "Végétal", "Minéral", "Céleste", "Psychique", "Chaotique", "Toxique", "Éthéré"]
+const array_region = ["Forêt", "Désert"]
 
 let exampleDropdown = new Axentix.Dropdown('#sort-dropdown', {
     animationType: 'fade',
     animationDuration: 200
 });
+
+////////////////EVENT////////////////////
 
 window.onresize = function(event) {
     let container_above = document.getElementById("top-container")
@@ -12,55 +17,71 @@ window.onresize = function(event) {
     SearchbarPlacement()
 }
 
-
 window.onscroll = function(event) {
     SearchbarPlacement()
 }
 
 document.getElementById("sort-Element").addEventListener('click', () => {
-    document.getElementById('sort-btn').textContent = 'Élément'
-    printcontent()
+    document.getElementById('sort-btn').textContent = 'Elément'
+    displaycontent()
 })
 
 document.getElementById("sort-Region").addEventListener('click', () => {
     document.getElementById('sort-btn').textContent = 'Région'
-    printcontent()
+    displaycontent()
 })
 
+///////////////FUNCTION//////////////////
+
 function printcontent() {
-    let sort_type = document.getElementById('sort-btn').textContent
     fetch(url).then((response) =>
         response.json().then((data) => {
             console.log(data)
-            console.log(sort_type);
-            document.getElementById("main-content").innerHTML = ``
-            if (sort_type == 'Élément') {
-                var array_element = ["Neutre", "Ardent", "Électrique", "Aquatique", "Végétal", "Minéral", "Céleste", "Psychique", "Chaotique", "Toxique", "Éthéré"]
-                for (let i = 0; i < array_element.length; i++) {
-                    document.getElementById("main-content").innerHTML +=
-                        `<div class="collapsible-trigger">
-                            <div id="collapsible-title">
-                                ${array_element[i]}
-                            </div>
-                            <img class = "bd-solid bd-orange dark-4" src="../images/black-img.png" id="collapsible-img" alt="test-png">
-                        </div>
-                    `
-                }
-            }
-            if (sort_type == "Région") {
-                var array_region = ["Forêt", "Désert"]
-                for (let i = 0; i < array_region.length; i++) {
-                    document.getElementById("main-content").innerHTML +=
-                        `<div class="collapsible-trigger">
+            var allcontent = ""
+            allcontent += `<div id="Region">`
+            for (let i = 0; i < array_region.length; i++) {
+                allcontent +=
+                    `<div id="${array_region[i]}" class="collapsible-trigger">
                             <div id="collapsible-title">
                                 ${array_region[i]}
                             </div>
                             <img class = "bd-solid bd-orange dark-4" src="../images/black-img.png" id="collapsible-img" alt="test-png">
                         </div>
                     `
-                }
             }
+            allcontent += `</div>`
+            allcontent += `<div id="Element">`
+            for (let i = 0; i < array_element.length; i++) {
+                allcontent +=
+                    `<div id="${array_element[i]}" class="collapsible-trigger">
+                            <div id="collapsible-title">
+                                ${array_element[i]}
+                            </div>
+                            <img class = "bd-solid bd-orange dark-4" src="../images/black-img.png" id="collapsible-img" alt="test-png">
+                        </div>
+                    `
+            }
+            allcontent += `</div>`
+            sort_content.innerHTML = allcontent
+            document.getElementById("Element").style = "display:none;"
+            document.getElementById("Region").style = "display:none;"
+            displaycontent()
         }));
+}
+
+function displaycontent() {
+    let sort_type = document.getElementById('sort-btn').textContent
+    console.log(sort_type);
+    if (sort_type == "Elément") {
+        console.log("display Element")
+        document.getElementById("Element").style = "display:flex; flex-direction: column;"
+        document.getElementById("Region").style = "display:none; "
+    }
+    if (sort_type == "Région") {
+        console.log("display Region")
+        document.getElementById("Element").style = "display:none; "
+        document.getElementById("Region").style = "display:flex; flex-direction: column;"
+    }
 }
 
 function SearchbarPlacement() {
