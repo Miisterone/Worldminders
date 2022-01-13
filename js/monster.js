2e03bb191c9cf160467800c2896953cfe0ee9e22
XFactors <adelincasez@icloud.com>
2022-01-13 09:10:07 +0100

    adding physical trait sort

--- js/monster.js
+++ js/monster.js
@@ -3,6 +3,8 @@ var searchbar_position = document.getElementById("searchbar").offsetTop - 100
 var sort_content = document.getElementById("main-content")
 const array_element = ["Neutre", "Ardent", "Électrique", "Aquatique", "Végétal", "Minéral", "Céleste", "Psychique", "Chaotique", "Toxique", "Éthéré"]
 const array_region = ["Forêt", "Désert"]
+const array_physical_trait = ["Canin", "Félin", "Oiseau", "Rongeur", "Reptile", "Insecte", "Poisson", "Éctoplasme", "Dragon", "Mutant"]
+
 var old_id;
 var old_parent_id;
 
@@ -33,18 +35,23 @@ async function printcontent() {
                     <img src="${img_src}" id="collapsible-img" alt="region.png">
                     </div>
                 </div>
-                <div id="${array_region[i]}" style="display:none; margin-top: 20px; padding: 15px; justify-content: center; flex-wrap: wrap;">
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
-                </div>
+                <div id="${array_region[i]}" style="display:none; margin-top: 20px; padding: 15px; justify-content: center; flex-wrap: wrap;">`
+                /*
+                for (let value in data) {
+                    if (data[value].region == array_region[i]) {
+                        allcontent += `<img src="${data[value].image}" style ="max-width: 200px; max-height: 200px; background: white; margin: 25px 25px"> `
+                    }
+                }
+*/
+        allcontent += `<div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+        <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>`
+                
+        allcontent += `</div>
             </div>
             `
 
@@ -72,7 +79,6 @@ async function printcontent() {
             }
         }
 
-
         allcontent += `<div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
                     <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
                     <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
@@ -81,6 +87,41 @@ async function printcontent() {
                     <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
                     <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>`
 
+        allcontent += `</div>
+            </div>
+            `
+    }
+    allcontent += `</div>`
+
+    allcontent += `<div id="Physical-trait">`
+    for (let i = 0; i < array_physical_trait.length; i++) {
+        var img_src = "../images/type_logo/" + array_physical_trait[i] + ".png"
+        allcontent +=
+            `<div style="width: 80vw; margin-top: 50px; border-radius: 60px; background-color: #977033;" id="parent-${array_physical_trait[i]}">
+                <div class="collapsible-trigger" onclick="func(${array_physical_trait[i]}, 'parent-${array_physical_trait[i]}')">
+                    <div style="width:100px;"></div>
+                    <div id="collapsible-title">
+                            ${array_physical_trait[i]}
+                    </div>
+                    <div class = "bd-solid bd-orange dark-4" id="collapsible-img-container">
+                    <img src="${img_src}" id="collapsible-img" alt="element.png">
+                    </div>
+                </div>
+                <div id="${array_physical_trait[i]}"style="display:none; margin-top: 20px; padding: 25px; justify-content: center; flex-wrap: wrap;">`
+/*
+        for (let value in data) {
+            if (data[value].physcical_trait == array_physical_trait[i]) {
+                allcontent += `<img src="${data[value].image}" style ="max-width: 200px; max-height: 200px; background: white; margin: 25px 25px"> `
+            }
+        }
+*/
+        allcontent += `<div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>
+                    <div style="width: 200px; height: 200px; background: black; margin: 25px 25px"></div>`
 
         allcontent += `</div>
             </div>
@@ -99,8 +140,10 @@ async function printcontent() {
 
     document.getElementById("Element").style.display = "none"
     document.getElementById("Region").style.display = "none"
+    document.getElementById("Physical-trait").style.display = "none"
     document.getElementById("Region").style.flexDirection = "column"
     document.getElementById("Element").style.flexDirection = "column"
+    document.getElementById("Physical-trait").style.flexDirection = "column"
 
     displaycontent()
 }
@@ -110,12 +153,17 @@ function displaycontent() {
     if (sort_type === "Elément") {
         document.getElementById("Element").style.display = "flex"
         document.getElementById("Region").style.display = "none"
+        document.getElementById("Physical-trait").style.display = "none"
     }
     if (sort_type === "Région") {
         document.getElementById("Region").style.display = "flex"
         document.getElementById("Element").style.display = "none"
-
-
+        document.getElementById("Physical-trait").style.display = "none"
+    }
+    if (sort_type === "Trait physique") {
+        document.getElementById("Physical-trait").style.display = "flex"
+        document.getElementById("Element").style.display = "none"
+        document.getElementById("Region").style.display = "none"
     }
 }
 
@@ -125,7 +173,12 @@ function SearchbarPlacement() {
     if (searchbar_position <= window.scrollY) {
         searchbar.style.position = 'fixed'
         searchbar.style.top = '100px'
-        document.getElementById('description').style.marginTop = '50px'
+        if (window.innerWidth >=600){
+            document.getElementById('description').style.marginTop = '50px'
+        } else {
+            document.getElementById('description').style.marginTop = '100px'
+        }
+    
     } else {
         searchbar.style.position = 'relative'
         searchbar.style.top = 'auto'
