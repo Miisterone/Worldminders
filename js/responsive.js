78ff4b58e7ea871c11b6ef578bdee2700f3c0fb3
WeZeOx <91544506+WeZeOx@users.noreply.github.com>
2022-02-09 13:40:12 +0100

    Push for responsive

---
+++ js/responsive.js
@@ -0,0 +1,18 @@
+const body = document.getElementById('body')
+const toActive1 = document.getElementById('main-sidenav')
+let clicked = false
+
+function responsive () {
+    console.log('click')
+    toActive1.classList.toggle('active')
+    body.style.overflow = "hidden"
+
+    if (clicked) {
+        body.style.removeProperty('overflow')
+        clicked = false;
+    } else {
+        body.style.overflow = "hidden"
+        clicked = true;
+    }
+
+}
