cf89b4475917eb6f5146834f13d9620885557a5a
Miisterone <miisterone27@gmail.com>
2022-03-03 22:37:43 +0100

    add(feature-api): add api

---
+++ api_worldminders/Data/MonsterContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using api_worldminders.Models;
+
+namespace api_worldminders.Data;
+
+public class MonsterContext : DbContext
+{
+    public MonsterContext (DbContextOptions<MonsterContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Monster> Monsters => Set<Monster>();
+}
